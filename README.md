fc577d640c5dfce106252e33d0113f52998377fb
John Giorshev <jagprogrammingprojects@gmail.com>
2019-01-07 22:30:38 -0500

    Added timeout function

---
+++ README.md
@@ -0,0 +1,14 @@
+![img](https://i.imgur.com/sMuGTol.jpg)
+# Substitute Middle Mouse Button
+
+On the first day of graphics class, I realized that the middle mouse button was **really** important
+for using Solid Edge (CAD software). My mouse didn't have one. Rather than driving to the store and buying a different mouse, I decided
+to make a program that runs in the background and maps an unused keyboard button to the middle mouse button instead.
+
+Here's how to use it:
+
+* The ' ` ' key mimics the middle mouse button
+* Pressing left-control + ' ` ' closes the program. A dialog confirms that the program closed.
+* The program only functions when Solid Edge is being used. If you press keys too many times outside of Solid Edge
+then the program will also close. The assumption is that the program was
+forgotten and left active by accident.
--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import pythoncom, pyHook, win32ui, pyautogui
 # Download the file corresponding to the python version being used (I'm using 3.6.6)
 # pip install the file
 
+exit_prompt_given = False
 middle_btn_state = False
 l_ctrl_state = False
+outside_key_count = 0
 
 def OnKeyboardEvent(event):
+    if exit_prompt_given or event.WindowName is None: # occurs when program is closing
+        return True
+
+    global outside_key_count
     # only work with key inputs if solid edge is being used
-    # event is none if the program is about to close
-    if event.WindowName is not None and event.WindowName.startswith("Solid Edge"):
+    if event.WindowName.startswith("Solid Edge"):
+        outside_key_count = 0 # reset outside key count once solid edge is being used
         global l_ctrl_state
         # Oem_3 is the '`' key, or lowercase '~'. Key near top left of keyboard
         if event.Key == "Oem_3":
@@ -23,15 +29,28 @@ def OnKeyboardEvent(event):
                 if middle_btn_state:
                     if l_ctrl_state:
                         # 4096 to bring project to top
-                        win32ui.MessageBox("The program is exiting.", "", 4096)
-                        exit()
+                        exit_with_prompt("")
                     pyautogui.mouseDown(button='middle')
                 else:
                     pyautogui.mouseUp(button='middle')
         elif event.Key == "Lcontrol":
             l_ctrl_state = event.MessageName == "key down"
+            if l_ctrl_state and middle_btn_state:
+                exit_with_prompt("")
+    else:
+        outside_key_count += 1
+        # increment key count if solid edge is not in focus
+        if outside_key_count == 50:
+            exit_with_prompt("You may have accidentally forgotten this program.\n")
+
     return True
 
+
+def exit_with_prompt(prefix):
+    global exit_prompt_given
+    exit_prompt_given = True
+    win32ui.MessageBox(prefix + "The program is exiting.", "Mouse Util", 4096)
+    exit()
 
 hm = pyHook.HookManager()
 # get both key up and key down events
